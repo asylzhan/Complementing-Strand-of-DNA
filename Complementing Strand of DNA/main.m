e02fcb07c9dbddec08ef40386c04c2ea880201d3
Asylzhan <asylzhan13@gmail.com>
2014-11-30 22:44:07 +0600

    Initial Commit

---
+++ Complementing Strand of DNA/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Complementing Strand of DNA
+//
+//  Created by asyl13 on 30.11.14.
+//  Copyright (c) 2014 Asylzhan Salimkhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
